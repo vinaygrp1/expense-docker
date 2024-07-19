598bac626fb13072a0b568c1139b1290cc0d6024
vinaygrp1 <vinaygrp1@gmail.com>
2024-07-19 12:49:49 +0530

    function

--- mysql/scripts/schema.sql
+++ mysql/scripts/schema.sql
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS transactions (
 
 CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'ExpenseApp@1';
 GRANT ALL ON transactions.* TO 'expense'@'%';
-FLUSH PRIVILEGES;
+FLUSH PRIVILEGES

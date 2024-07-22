40e7e3b45993843500dd19cb518ff344298d5978
vinaygrp1 <vinaygrp1@gmail.com>
2024-07-22 15:11:53 +0530

    function

--- install-docker.sh
+++ install-docker.sh
@@ -6,7 +6,7 @@ N="\e[0m"
 sudo su -
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl status docker
 systemctl enable docker
